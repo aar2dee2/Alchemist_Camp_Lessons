68d3bb51c70707e747bf237d5a2af7d431a364fd
Sam <85004512+aar2dee2@users.noreply.github.com>
2021-08-25 11:58:28 +0000

    Md>>html parser created. Whoop!

--- L4-Mini_MD/read_md.exs
+++ L4-Mini_MD/read_md.exs
@@ -2,27 +2,31 @@ parent = "/home/runner/AlchemistCampLessons"
 folder = IO.gets("Enter the name of the folder you want to scan for image files ..>") |> String.trim()
 dir = parent <> "/" <> folder
 
-if File.dir?(Path.absname(dir)) do
+{outcome, files} = if File.dir?(Path.absname(dir)) do
   File.cd(Path.absname(dir))
-  {_, files} = File.ls()
+  File.ls()
 else
-  IO.puts("#{folder} is not a valid folder\n")
+  {:invalid_folder, []}
 end
 
-case files do
+{result, markdowns, location} = case files do
   [] -> IO.puts "There are no files in the folder specified"
-  _ -> {result, markdowns, location} = MiniMarkdown.scan4md(files, Path.absname(dir))
-      IO.puts("Choose which file you want to convert to html: \n")
-      for x <- 1..(length(markdowns) - 1), IO.puts("#{x + 1}. #{Enum.fetch!(markdowns, x)}")
-      choice = IO.gets("Enter the corresponding file number: ") |> String.trim() |> String.to_integer()
+        {:no_files, [], dir}
+  _ -> MiniMarkdown.scan4md(files, Path.absname(dir))
 end
 
-md_choice = Enum.fetch!(markdowns, choice)
-case File.read(md_choice) do
-  {:ok, body} -> body
-  {:error, reason} -> IO.puts(~s/Could not open the file #{filename}\n/)
-                      IO.puts ~s("#{:file.format_error reason}"\n)
-end
+if markdowns != [] do
+  IO.puts("Choose which file you want to convert to html: \n")
+  for x <- 0..(length(markdowns) - 1), do: IO.puts("#{x + 1}. #{Enum.fetch!(markdowns, x)}\n")
+  choice = IO.gets("Enter the corresponding file number: ") |> String.trim() |> String.to_integer()
 
-output = MiniMarkdown.to_html(body)
-File.write("#{Regex.replace(~r/\.md/, md_choice, "")}.html",output)
+  md_choice = Enum.fetch!(markdowns, choice - 1)
+  body = case File.read(md_choice) do
+          {:ok, body} -> body
+          {:error, reason} -> ""
+         end
+  output = MiniMarkdown.to_html(body)
+  File.write("#{Regex.replace(~r/\.md/, md_choice, "")}.html",output)
+else
+  IO.puts "There are no markdown files in the selecrteed folder"
+end
