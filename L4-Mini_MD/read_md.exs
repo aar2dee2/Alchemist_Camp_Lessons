1d311ba7891eea575274d1a8cba687b692d27d2e
Sam <85004512+aar2dee2@users.noreply.github.com>
2021-08-25 10:52:38 +0000

    Extended challenge - get name of md from user and create output html file after parsing the markdown text

--- L4-Mini_MD/read_md.exs
+++ L4-Mini_MD/read_md.exs
@@ -5,14 +5,14 @@ dir = parent <> "/" <> folder
 if File.dir?(Path.absname(dir)) do
   File.cd(Path.absname(dir))
   {_, files} = File.ls()
-  {result, markdowns, dir} = scan4md(files, Path.absname(dir))
 else
   IO.puts("#{folder} is not a valid folder\n")
 end
 
-case markdowns do
-  [] -> IO.puts "There are no markdown files in the folder specified"
-  _ -> IO.puts("Choose which file you want to convert to html: \n")
+case files do
+  [] -> IO.puts "There are no files in the folder specified"
+  _ -> {result, markdowns, location} = MiniMarkdown.scan4md(files, Path.absname(dir))
+      IO.puts("Choose which file you want to convert to html: \n")
       for x <- 1..(length(markdowns) - 1), IO.puts("#{x + 1}. #{Enum.fetch!(markdowns, x)}")
       choice = IO.gets("Enter the corresponding file number: ") |> String.trim() |> String.to_integer()
 end
@@ -25,3 +25,4 @@ case File.read(md_choice) do
 end
 
 output = MiniMarkdown.to_html(body)
+File.write("#{Regex.replace(~r/\.md/, md_choice, "")}.html",output)
